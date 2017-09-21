492375e648e8023cf010c8dcd22f17ed4ae6c769
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2017-09-20 19:06:26 -0500

    Cell added

---
+++ Mi Primera Tabla/cellMainTable.h
@@ -0,0 +1,13 @@
+//
+//  cellMainTable.h
+//  Mi Primera Tabla
+//
+//  Created by Walter Gonzalez Domenzain on 20/09/17.
+//  Copyright © 2017 wgdomenzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+
+@end
