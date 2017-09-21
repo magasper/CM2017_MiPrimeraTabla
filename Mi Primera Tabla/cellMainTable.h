4746dc52a415a0863f31e00d265a1d209730f1c5
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2017-09-20 19:11:40 -0500

    Cell objects were connected to m file

--- Mi Primera Tabla/cellMainTable.h
+++ Mi Primera Tabla/cellMainTable.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface cellMainTable : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgUser;
+@property (strong, nonatomic) IBOutlet UILabel *lblName;
+@property (strong, nonatomic) IBOutlet UILabel *lblAge;
 
 @end
